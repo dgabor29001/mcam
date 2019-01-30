2d456acff0720b9ff0469657097cad8d0c4c6236
dgabor_gecor <dgabor@gecorweb.com>
2019-01-30 13:50:15 +0100

    add SuppressLint

--- src/android/WowCamera.java
+++ src/android/WowCamera.java
@@ -1,6 +1,7 @@
 package org.apache.cordova.camera;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
